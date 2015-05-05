f736553b7893970f2df5b382b63c737fe4ee5457
Ruchi Agarwal <ruchi.agarwalcse@gmail.com>
2015-05-05 14:10:04 -0700

    Initial Commit
    Project Structure

---
+++ src/main/java/FacebookSync/Application.java
@@ -0,0 +1,18 @@
+package FacebookSync;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+import com.restfb.scope.UserDataPermissions;
+import com.restfb.types.FriendList;
+import com.restfb.types.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
