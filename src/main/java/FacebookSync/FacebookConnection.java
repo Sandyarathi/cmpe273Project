2164b6e564467e6728ba0e624f4cda15a0220a8d
Ruchi Agarwal <ruchi.agarwalcse@gmail.com>
2015-05-05 15:21:00 -0700

    FB Connectivity
    Few Changes

--- src/main/java/FacebookSync/FacebookConnection.java
+++ src/main/java/FacebookSync/FacebookConnection.java
@@ -39,12 +39,6 @@ public class FacebookConnection {
     @RequestMapping(value = "welcome.jsp", method = RequestMethod.GET)
     public void  facebookInit(@RequestParam(value="code") String code) throws IOException{
         token = getFacebookUserToken(code, redirectUrl);
-
-
-        //return rPosts;
-        //System.out.println(fList);
-        //return arr;
-        //return "Ruchi";
     }
 
     private FacebookClient.AccessToken getFacebookUserToken(String code, String redirectUrl) throws IOException {
@@ -57,28 +51,6 @@ public class FacebookConnection {
     {
         ArrayList<String> arr = new ArrayList<String>();
         HashMap <String,ArrayList<String>> hash  = new HashMap <String,ArrayList<String>>();
-        //String accessToken = token.getAccessToken();
-        //Date expires = token.getExpires();
-        //FacebookClient fbClient = new DefaultFacebookClient(accessToken,Version.VERSION_2_2);
-        //User me = fbClient.fetchObject("me",com.restfb.types.User.class);
-
-        //arr.add(accessToken);
-        /*Date oneYearAgo =
-                new Date(System.currentTimeMillis() - 1000L * 60L * 60L * 24L * 800L);
-        Connection<Post> rPosts = fbClient.fetchConnection("me/posts", Post.class, Parameter.with("until", "yesterday"), Parameter.with(
-                "since", oneYearAgo));
-        for (Post p: rPosts.getData()){
-            arr.add(p.getMessage());
-        }
-        while (rPosts.hasNext()) {
-            rPosts = fbClient.fetchConnectionPage(rPosts.getNextPageUrl(),Post.class);
-            for (Post p: rPosts.getData()){
-                arr.add(p.getMessage());
-            }
-        } */
-
-        //return rPosts;
-        //System.out.println(fList);
         arr.add("Ruchi");
         arr.add("Piyush");
         arr.add("Mom");
@@ -88,27 +60,6 @@ public class FacebookConnection {
         arr.add("Tim");
         hash.put("February 2014", arr);
         return hash;
-        //return "Ruchi";
     }
 
-   /* public static void main(String[] args) {
-        //String appId = "403024159903643";
-        //String appSecret = "15b0bf950c65802d807eb71ac932820a";
-
-        FacebookClient fbClient = new DefaultFacebookClient("CAACEdEose0cBAIckK9r7355XDPgyetocsqKZCkyQURHMj8g9ZACtBpkPsLqoAt2VIc7V5S6jROqjhfsRl3yoDH9naXfiZBv4N6TSl8r4DaDQnURoM5gwGDJ10dR1ai30mC6LgUtRZB56gbi8aeO0H6m6BSGcr9ueZBSSi0nNpoXvCSXalMl4iVPMaeQNqZBwxkCKVnxhllvO3vRw6oycZACUeVeIcwwXeUZD");
-        User me = fbClient.fetchObject("749936265121703",com.restfb.types.User.class);
-        FriendList fList = fbClient.fetchObject("749936265121703",com.restfb.types.FriendList.class);
-        System.out.println(me.getEducation());
-        System.out.println();
-        BatchRequest meRequest = new BatchRequestBuilder("me").build();
-        BatchRequest friendsRequest = new BatchRequestBuilder("me/friends").build();
-        List<BatchResponse> results = fbClient.executeBatch(meRequest, friendsRequest);
-        BatchResponse resultUser = results.get(0);
-        BatchResponse resultFriendList = results.get(1);
-        JsonMapper jsonMapper = new DefaultJsonMapper();
-        User me = jsonMapper.toJavaObject(resultUser.getBody(), User.class);
-        FriendList fList =  jsonMapper.toJavaObject(resultFriendList.getBody(), FriendList.class);
-        System.out.println(fList);
-
-    }        */
 }
